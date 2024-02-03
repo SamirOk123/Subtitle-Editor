44b00d00d4a7bd028b6eb7fa11732a05d6e41454
samir-finsire <samir@finsire.com>
2024-02-04 01:53:51 +0530

    Added scroll bar in editor screen

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xff922C29);
+const Color kSecondaryColor = Color(0xffF2F0E3);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:subtitle_editor/constants.dart';
 import 'package:subtitle_editor/home.dart';
 
 void main() {
@@ -10,9 +11,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color customColor = kPrimaryColor;
+
+    MaterialColor customSwatch = MaterialColor(
+      customColor.value,
+      <int, Color>{
+        50: customColor.withOpacity(0.1),
+        100: customColor.withOpacity(0.2),
+        200: customColor.withOpacity(0.3),
+        300: customColor.withOpacity(0.4),
+        400: customColor.withOpacity(0.5),
+        500: customColor.withOpacity(0.6),
+        600: customColor.withOpacity(0.7),
+        700: customColor.withOpacity(0.8),
+        800: customColor.withOpacity(0.9),
+        900: customColor,
+      },
+    );
     return MaterialApp(
       home: const HomeScreen(),
       theme: ThemeData(
+        primarySwatch: customSwatch,
+        scaffoldBackgroundColor: kSecondaryColor,
+        appBarTheme: const AppBarTheme(backgroundColor: kPrimaryColor),
         useMaterial3: false,
       ),
       debugShowCheckedModeBanner: false,
